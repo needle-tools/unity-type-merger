8357c338ab3d42f67daba727b6e7491b04598895
Marcel Wiessler <marcel@gaisterhand.de>
2023-03-12 02:30:41 +0100

    Adding the component at runtime works in build

--- projects/ClassMerger/Assets/NewBehaviourScript.cs
+++ projects/ClassMerger/Assets/NewBehaviourScript.cs
@@ -1,18 +1,17 @@
-// using DefaultNamespace;
-// using UnityEngine;
-//
-// public class NewBehaviourScript : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     private void Start()
-//     {
-//         var comp = FindObjectOfType<MyComponent>();
-//         Debug.Log(comp);
-//     } 
-//
-//     // Update is called once per frame
-//     void Update()
-//     {
-//           
-//     }
-// }
+using DefaultNamespace;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameObject.AddComponent<MyComponent>();
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+}
